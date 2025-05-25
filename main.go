a5acf5bdf8ca101c311efed90a37adf220798d52
satyam709 <satyamchauhan709@gmail.com>
2025-05-25 17:20:41 +0530

    adds go-testcontainers and current_time test

--- main.go
+++ main.go
@@ -1,13 +1,5 @@
 package main
 
-import (
-    "fmt"
-)
 func main() {
-    err := SetFakeTime(1747539000000) // Example timestamp in milliseconds
-    if err != nil {
-        fmt.Printf("Failed to set fake time: %v\n", err)
-        return
-    }
-    fmt.Println("Successfully set fake time")
+    
 }
